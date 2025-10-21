6e325487d9fe9b6bfc68415b98884847791e7ef4
Miguel Angel <miguelangelo23404@gmail.com>
2025-10-21 15:56:40 -0500

    New variable in quote

--- api/quotes.js
+++ api/quotes.js
@@ -1,19 +1,12 @@
-import express from "express";
 import fetch from "node-fetch";
 
-const app = express(); 
-
-app.get("/api/quotes", async(req, res)=>{
-    try {
-        const respuesta = await fetch("https://zenquotes.io/api/random"); 
-        const data = await respuesta.json(); 
-        res.set("Access-Control-Allow-Origin", "*"); 
-        res.json(data); 
-        
-    } catch (error) {
-        res.status(500).json({error: "Error getting results"})    
-        
-    }
-})
-
-app.listen(3000, ()=>console.log("Servidor en http://localhost:3000"))
+export default async function handler(req, res) {
+  try {
+    const respuesta = await fetch("https://zenquotes.io/api/random");
+    const data = await respuesta.json();
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Error obteniendo la frase" });
+  }
+}
